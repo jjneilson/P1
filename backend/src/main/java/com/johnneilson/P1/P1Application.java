4bcff203c51a9edb124180dc99c5aa963227b2aa
neils <neilson.john38781@gmail.com>
2024-12-17 15:28:12 -0500

    initial commit

---
+++ backend/src/main/java/com/johnneilson/P1/P1Application.java
@@ -0,0 +1,13 @@
+package com.johnneilson.P1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1Application.class, args);
+	}
+
+}
