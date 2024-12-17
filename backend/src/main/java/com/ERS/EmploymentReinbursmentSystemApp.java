b32f4907c9c29c867b05e7c302f9ba143e670aba
neils <neilson.john38781@gmail.com>
2024-12-17 15:40:02 -0500

    move of files and renames

--- backend/src/main/java/com/ERS/EmploymentReinbursmentSystemApp.java
+++ backend/src/main/java/com/ERS/EmploymentReinbursmentSystemApp.java
@@ -1,13 +1,13 @@
-package com.johnneilson.P1;
+package com.ERS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class P1Application {
+public class EmploymentReinbursmentSystemApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(P1Application.class, args);
+		SpringApplication.run(EmploymentReinbursmentSystemApp.class, args);
 	}
 
 }
