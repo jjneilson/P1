6ad4d4250c01a73422b62779773cc38ad8ac5762
neils <neilson.john38781@gmail.com>
2024-12-17 16:45:18 -0500

    more file moves and creation of entity objects

---
+++ backend/src/main/java/com/ERS/entity/Reinbursment.java
@@ -0,0 +1,108 @@
+
+
+
+
+@Entity
+@Table(name = "reinbursment")
+public class Reinbursment {
+    
+    /**
+     * The unique identifier for the reinbursment.
+     */
+    @Column(name = "reimb_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int reimbId;
+
+    /**
+     * A brief description of the reimbursement.
+     */
+    private String description;
+
+    /**
+     * Amount of reimbursment.
+     */
+    private int amount;
+    
+    /**
+     * Status of the reinbursment.
+     */
+    private String status;
+    
+    /**
+     * Id of user reinbursment belongs to.
+     */
+    private int userId;
+
+    /**
+     * Default constructor for the Reinbursment class.
+     * Initializes a new instance of the Reinbursment class.
+     */
+    public Reinbursment() {
+    }
+
+    /**
+     * Constructs a new Reinbursment object with the specified description, amount, status, and userId.
+     *
+     * @param description A brief description of the reimbursement.
+     * @param amount The amount of the reimbursement.
+     * @param status The current status of the reimbursement.
+     * @param userId The ID of the user associated with the reimbursement.
+     */
+
+    public Reinbursment(String description, int amount, String status, int userId) {
+        this.description = description;
+        this.amount = amount;
+        this.status = status;
+        this.userId = userId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Returns a string representation of the Reinbursment object.
+     * The string representation includes the description, amount, status, and userId fields.
+     *
+     * @return a string representation of the Reinbursment object
+     */
+    @Override
+    public String toString() {
+        return "Reinbursment{" +
+                "description=" + description + '\\' +
+                "amount" + amount + '\\' +
+                "status" + status + '\\' +
+                "userId" + userId + '\\' +
+                '}';
+    }
+
+}
---
+++ backend/src/main/java/com/ERS/entity/User.java
@@ -0,0 +1,92 @@
+public class User {
+    
+    private int userId;    
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    public User() {
+    }
+
+    public User(int userId, String firstName, String lastName, String username, String password, String role) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * Returns a string representation of the User object.
+     * The string includes the userId, firstName, lastName, username, password, and role fields.
+     *
+     * @return a string representation of the User object
+     */
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId + '\\' +
+                "firstName=" + firstName + '\\' +
+                "lastName=" + lastName + '\\' +
+                "username=" + username + '\\' +
+                "password=" + password + '\\' +
+                "role=" + role + '\\' +
+                '}';
+    }
+}
