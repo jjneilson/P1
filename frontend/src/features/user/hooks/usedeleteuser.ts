da2f00697f1e50ec0a3fd540b2d227ff2393b65b
neils <neilson.john38781@gmail.com>
2025-01-06 08:15:05 -0500

    added query to function

--- frontend/src/features/user/hooks/usedeleteuser.ts
+++ frontend/src/features/user/hooks/usedeleteuser.ts
@@ -1,15 +1,17 @@
 import { axiosInstance } from "@/lib/axios-config";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 export function useDeleteUser() {
+    const queryClient = useQueryClient();
+    
     return useMutation({
         mutationFn: async (userid: number) => {
             const resp = await axiosInstance.delete(`/users/delete/${userid}`, { headers: { Authorization: localStorage.getItem('token') } });
             return resp.data;
         },
         onSuccess: () => {
-            window.location.reload();
+            queryClient.invalidateQueries({ queryKey: ["auth"] });
             toast.success('User deleted successfully');
         },
         onError: () => {
